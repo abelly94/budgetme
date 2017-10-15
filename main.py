605cae7826c7146f4446e08b1f046141bb9d1400
Alex Belly <abelly94@gmail.com>
2017-10-14 22:11:41 -0400

    Added configureation details for the primary app window.

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ class App(tk.Frame):
         self.create_widgets()
 
 
+#Configures the master GUI window with a title and
+#fixed dimensions. Plan to make a resizeable window
+#at a later date
+
+    def configure_gui(self):
+        self.master.title("budgetme")
+        self.master.geometry("1000x1000")
+        self.master.resizeable(False,False)
+
+
 root = tk.Tk()
 app=App(master=root)
 app.mainloop()
